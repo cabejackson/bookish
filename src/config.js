f6960029c1ec852d3f4a4b64c14eb55b680257cc
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-10-14 02:16:00 -0400

    /api

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     // API_ENDPOINT: "http://localhost:8000/api",
-    API_ENDPOINT: "https://rocky-woodland-08613.herokuapp.com",
+    API_ENDPOINT: "https://rocky-woodland-08613.herokuapp.com/api",
     TOKEN_KEY: 'bnbClientAuthToken', //is this just some secret value?
 }
 
